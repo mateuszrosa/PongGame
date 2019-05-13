4442205c5c177f1c4b9d698e4380e2756953ea3c
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-13 17:52:50 +0200

    added player and ai paddles

--- js/app.js
+++ js/app.js
@@ -6,7 +6,29 @@ canvas.height = 500;
 
 const cw = canvas.width;
 const ch = canvas.height;
+
 const ballSize = 20;
+let ballX = cw / 2 - ballSize / 2;
+let ballY = ch / 2 - ballSize / 2;
+
+const paddleWidth = 20;
+const paddleHeight = 100;
+
+const playerX = 70;
+const aiX = 910;
+
+const playerY = 200;
+const aiY = 200;
+
+const player = () => {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);
+}
+
+const ai = () => {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(aiX, aiY, paddleWidth, paddleHeight);
+}
 
 const table = () => {
     ctx.fillStyle = "black";
@@ -15,7 +37,10 @@ const table = () => {
 
 const ball = () => {
     ctx.fillStyle = "white";
-    ctx.fillRect(cw / 2 - ballSize / 2, ch / 2 - ballSize / 2, ballSize, ballSize);
+    ctx.fillRect(ballX, ballY, ballSize, ballSize);
 }
+
 table();
 ball();
+player();
+ai();
