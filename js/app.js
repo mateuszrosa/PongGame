952b1b133a41c02a12b95810cb3868d7ecabab7e
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-13 17:30:35 +0200

    ball added

--- js/app.js
+++ js/app.js
@@ -6,10 +6,16 @@ canvas.height = 500;
 
 const cw = canvas.width;
 const ch = canvas.height;
+const ballSize = 20;
 
 const table = () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, cw, ch);
 };
 
+const ball = () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(cw / 2 - ballSize / 2, ch / 2 - ballSize / 2, ballSize, ballSize);
+}
 table();
+ball();
